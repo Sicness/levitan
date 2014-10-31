f0bf6f02d9ed320926e8227f795101e1e79b9307
Vlad Slepukhin <slp.vld@gmail.com>
2014-10-31 12:40:51 +0300

    Add ImportError handling

--- pluginInitializer.py
+++ pluginInitializer.py
@@ -10,6 +10,8 @@ def create_initial_plugin_list(cfg):
     print('\nLoading plugins:')
     # Create plugin names list
     plugins = cfg['plugins'].keys()
+    print('\n'.join(plugins))
+
     return plugins
 
 
@@ -20,23 +22,27 @@ def initialize_plugins(plugin_name_list, cfg):
     :param cfg: configuration file
     :return: plugins: list of checked plugin instances
     """
-    print plugin_name_list
     print('\nInitializing and checking plugins:')
     plugins = []
     for name in plugin_name_list:
-        module = __import__('Plugins.' + name)
-        plugin_class = getattr(module, name)
-        obj = getattr(plugin_class, filter(lambda x: x == name, dir(plugin_class))[0])
-        if inspect.isclass(obj):
-            instance = obj(*cfg['plugins'][name])
+        print('Initializing: %s' % name)
+        try:
+            module = __import__('Plugins.' + name)
+            plugin_class = getattr(module, name)
+            obj = getattr(plugin_class, filter(lambda x: x == name, dir(plugin_class))[0])
+            if inspect.isclass(obj):
+                instance = obj(*cfg['plugins'][name])
 
-            print('Checking: %s\n' % instance.hello())
-            check_response = instance.check_plugin_config()
-            if check_response['status']:
-                print('%s OK.' % instance.hello())
-                plugins.append(instance)
+                print('Checking: %s' % instance.hello())
+                check_response = instance.check_plugin_config()
+                if check_response['status']:
+                    print('%s OK.\n' % instance.hello())
+                    plugins.append(instance)
+                else:
+                    print('%s FAIL. Error: %s.\n' % (instance.hello(), check_response['errorMessage']))
             else:
-                print('%s FAIL. Error: %s.' % (instance.hello(), check_response['errorMessage']))
-        else:
-            print('Plugin with name %s failed to initialize: unable to create instance.' % name)
+                print('Plugin with name %s failed to initialize: unable to create instance.\n' % name)
+        except ImportError as e:
+            print('Plugin failed to be imported as: %s\n' % e)
+
     return plugins
