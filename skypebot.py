c4672c1c962c2185ccded5a716e2b6e9e0f870b1
Vlad Slepukhin <slp.vld@gmail.com>
2014-11-05 20:34:56 +0300

    Turn off MarkAsSeen() due to exception (investigating)

--- skypebot.py
+++ skypebot.py
@@ -17,7 +17,7 @@ class SkypeBot:
 
     def MessageStatus(self, msg, status):
         print("INCOMING> %s" % msg.Body)
-        msg.MarkAsSeen()
+        # msg.MarkAsSeen()
         if status == Skype4Py.cmsReceived:
             for plugin in self.plugins:
                 r = plugin.plugin_process_request(msg)
